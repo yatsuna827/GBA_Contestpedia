4b1a854f2b3fb2fb160a03ca1f3ea67b02c4d810
yatsuna827 <yatsuna_827@yahoo.co.jp>
2023-11-26 03:33:06 +0900

    fix: captionを使うと視覚的な崩れが発生したのでaria-labelに変更

--- src/pages/moves/indexPage.tsx
+++ src/pages/moves/indexPage.tsx
@@ -41,8 +41,7 @@ const TypeSection: React.FC<{ appealType: AppealType }> = ({ appealType }) => {
 
 const MovesTable: React.FC<{ moves: readonly Move[] }> = ({ moves }) => {
   return (
-    <table>
-      <caption>わざ一覧</caption>
+    <table aria-label="わざ一覧">
       <thead>
         <tr>
           <th>わざ名</th>
